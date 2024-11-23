d6bae7ffebc27926d8e722d9d3e8098af7f9c540
radithya-mahesa <radithya.syabil@gmail.com>
2024-11-23 22:47:15 +0700

    radit|231124|feat: add /j and /w path for page view

---
+++ src/views/nword.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex flex-row min-h-screen justify-center items-center">
+        <router-link to="/j/" tag="button">
+            <h1 class="bg-blue-500 p-3 rounded-[50px] text-white">
+                pindah ke Jword
+            </h1>
+        </router-link>
+    </div>
+</template>
+
+<script></script>
